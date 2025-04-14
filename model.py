7c896c5e3e5fa09a9d2d1b4285414cbe6091fb5d
s24256-pj <s24256@pjwstk.edu.pl>
2025-04-14 18:23:41 +0200

    add transformer model and data

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import optuna
 import random
-
+import os
 
 
 class WeatherModel(nn.Module):
@@ -59,24 +59,13 @@ class WeatherModel(nn.Module):
 
         return self.output_layer(decoder_output)
 
-def main():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}")
+def prepare_data(data, input_seq_length, output_seq_length, batch_size,target_features):
+    data = pd.read_csv(data)
+    data = data.select_dtypes(include=['int', 'float'])
 
-    data = pd.read_csv('weather_data_full.csv')
-    data.drop(columns=['Station Name', 'Date'], inplace=True)
     data.fillna(0, inplace=True)
 
     input_features = data.columns.tolist()
-    target_features = [
-        "Average Daily Total Cloud Cover [oktas]",
-        "Average Daily Wind Speed [m/s]",
-        "Average Daily Temperature [°C]",
-        "Average Daily Relative Humidity [%]",
-        "Average Daily Station-Level Pressure [hPa]",
-        "Daily Precipitation Sum [mm]",
-        "Night Precipitation Sum [mm]"
-    ]
 
     target_data = data[target_features]
     input_data = data[input_features]
@@ -90,17 +79,6 @@ def main():
     input_tensor = torch.tensor(scaled_input_data, dtype=torch.float32)
     target_tensor = torch.tensor(scaled_target_data, dtype=torch.float32)
 
-    def create_sequences(data, target, input_seq_length, output_seq_length):
-        X, Y = [], []
-        for i in range(len(data) - input_seq_length - output_seq_length):
-            X.append(data[i:i + input_seq_length, :])
-            Y.append(target[i + input_seq_length:i + input_seq_length + output_seq_length, :])
-        return torch.stack(X), torch.stack(Y)
-
-
-    input_seq_length = 7
-    output_seq_length = 7
-
     X, Y = create_sequences(input_tensor, target_tensor, input_seq_length, output_seq_length)
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=False)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, shuffle=False)
@@ -109,109 +87,189 @@ def main():
     test_dataset = TensorDataset(X_test, y_test)
     val_dataset = TensorDataset(X_val, y_val)
 
-    optuna.logging.set_verbosity(optuna.logging.INFO)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    def objective(trial):
+    return train_loader, val_loader, test_loader, X, Y
 
-        seed = 42
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        random.seed(seed)
-        np.random.seed(seed)
+def create_sequences(data, target, input_seq_length, output_seq_length):
+    X, Y = [], []
+    for i in range(len(data) - input_seq_length - output_seq_length):
+        X.append(data[i:i + input_seq_length, :])
+        Y.append(target[i + input_seq_length:i + input_seq_length + output_seq_length, :])
+    return torch.stack(X), torch.stack(Y)
 
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+def train_model(model, train_loader, val_loader, num_epochs,device, learning_rate):
+    mse_loss = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        num_heads = trial.suggest_categorical('num_heads', [2,4,8])
-        num_layers = trial.suggest_int('num_layers', 2, 6)
-        embed_dim = trial.suggest_categorical('embed_dim', [32, 64,128,256,512])
-        learning_rate = trial.suggest_categorical('learning_rate', [0.00001,0.0001,0.001,0.01] )
-        dropout = trial.suggest_categorical('dropout', [0.1,0.2,0.3,0.4, 0.5])
-        dim_feedforward_encoder = trial.suggest_categorical('dim_feedforward_encoder', [32, 64,128,256,512,1024,2048])
-        dim_feedforward_decoder = trial.suggest_categorical('dim_feedforward_decoder', [32, 64,128,256,512,1024,2048])
-        batch_size = trial.suggest_categorical('batch_size', [32,64])
+    train_losses = []
+    val_losses = []
 
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_loss = 0
 
-        model = WeatherModel(input_dim=X.shape[2], embed_dim=embed_dim, num_heads=num_heads, num_layers=num_layers, seq_len=input_seq_length,
-                             output_features=Y.shape[2], dropout=dropout, dim_feedforward_encoder=dim_feedforward_encoder, dim_feedforward_decoder=dim_feedforward_decoder)
-        model.to(device)
+        for X_batch, y_batch in train_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
 
-        mse_loss = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+            optimizer.zero_grad()
+            tgt_input = y_batch[:, :-1, :].clone()
+            output = model(X_batch, tgt_input)
 
-        num_epochs = 15
-        train_losses = []
-        val_losses = []
+            loss = mse_loss(output, y_batch[:, 1:, :])
+            loss.backward()
+            optimizer.step()
 
-        print(X.shape, Y.shape)
+            epoch_loss += loss.item()
 
-        for epoch in range(num_epochs):
-            model.train()
-            epoch_loss = 0
+        train_losses.append(epoch_loss / len(train_loader))
 
-            for X_batch, y_batch in train_loader:
-                X_batch, y_batch = X_batch.to(device), y_batch.to(device)
-
-                optimizer.zero_grad()
-                tgt_input = y_batch[:, :-1, :].clone()
-                output = model(X_batch, tgt_input)
-
-                loss = mse_loss(output, y_batch[:, 1:, :])
-                loss.backward()
-                optimizer.step()
-
-                epoch_loss += loss.item()
-
-            train_losses.append(epoch_loss / len(train_loader))
-
-            print(f"Epoch {epoch}, Loss: {train_losses[-1]:.4f}")
-
-            model.eval()
-            with torch.no_grad():
-                val_loss = 0
-                all_predictions = []
-                all_targets = []
-
-                for X_batch, y_batch in val_loader:
-                    X_batch, y_batch = X_batch.to(device), y_batch.to(device)
-                    tgt_input = y_batch[:, :-1, :]
-                    predictions = model(X_batch, tgt_input)
-                    loss = mse_loss(predictions, y_batch[:, 1:, :])
-                    val_loss += loss.item()
-
-                    all_predictions.append(predictions.cpu().numpy())
-                    all_targets.append(y_batch[:, 1:, :].cpu().numpy())
-
-                val_losses.append(val_loss / len(val_loader))
-                if num_epochs % 5 == 0:
-                    print(f"Validation Loss: {val_losses[-1]:.4f}")
+        print(f"Epoch {epoch}, Loss: {train_losses[-1]:.4f}")
 
         model.eval()
         with torch.no_grad():
-            test_loss = 0
-            all_predictions = []
-            all_targets = []
+            val_loss = 0
 
-            for X_batch, y_batch in test_loader:
+            for X_batch, y_batch in val_loader:
                 X_batch, y_batch = X_batch.to(device), y_batch.to(device)
                 tgt_input = y_batch[:, :-1, :]
                 predictions = model(X_batch, tgt_input)
                 loss = mse_loss(predictions, y_batch[:, 1:, :])
-                test_loss += loss.item()
+                val_loss += loss.item()
 
-                all_predictions.append(predictions.cpu().numpy())
-                all_targets.append(y_batch[:, 1:, :].cpu().numpy())
+            val_losses.append(val_loss / len(val_loader))
+            if num_epochs % 5 == 0:
+                print(f"Validation Loss: {val_losses[-1]:.4f}")
 
-            print(f"Test Loss: {test_loss:.4f}")
+    return model, train_losses, val_losses
 
-            return val_loss / len(val_loader)
+def evaluate_model(model, test_loader, device):
+    mse_loss = nn.MSELoss()
 
+    model.eval()
+    with torch.no_grad():
+        test_loss = 0
+        all_predictions = []
+        all_targets = []
 
-    study = optuna.create_study(direction="minimize", study_name="weather_transformer_optimization")
+        for X_batch, y_batch in test_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            tgt_input = y_batch[:, :-1, :]
+            predictions = model(X_batch, tgt_input)
+            loss = mse_loss(predictions, y_batch[:, 1:, :])
+            test_loss += loss.item()
+
+            all_predictions.append(predictions.cpu().numpy())
+            all_targets.append(y_batch[:, 1:, :].cpu().numpy())
+
+        print(f"Test Loss: {test_loss:.4f}")
+        return test_loss, all_predictions, all_targets
+
+def visualisation(train_losses, val_losses, all_predictions, all_targets):
+    plt.figure(figsize=(10, 7))
+    plt.plot(train_losses, label="Train Loss", color="blue")
+    plt.plot(val_losses, label="Validation Loss", color="red")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Training and Testing Loss")
+    plt.legend()
+    plt.show()
+
+    all_predictions = np.concatenate(all_predictions, axis=0)
+    all_targets = np.concatenate(all_targets, axis=0)
+
+    num_samples = 5
+
+    plt.figure(figsize=(12, 7))
+
+    for i in range(num_samples):
+        plt.subplot(num_samples, 1, i + 1)
+        plt.plot(all_targets[i, :, 2], label="Actual", color="green")
+        plt.plot(all_predictions[i, :, 2], label="Predicted", linestyle="dashed", color="orange")
+        plt.ylabel("Value")
+        plt.legend()
+        plt.title(f"Sample {i + 1}")
+
+    plt.xlabel("Time Step")
+    plt.show()
+
+def set_seed(seed=42):
+    seed = 42
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def objective(trial):
+    set_seed()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    num_heads = trial.suggest_categorical('num_heads', [2, 4, 8])
+    num_layers = trial.suggest_int('num_layers', 2, 6)
+    embed_dim = trial.suggest_categorical('embed_dim', [32, 64, 128, 256, 512])
+    learning_rate = trial.suggest_categorical('learning_rate', [0.00001, 0.0001, 0.001, 0.01])
+    dropout = trial.suggest_categorical('dropout', [0.1, 0.2, 0.3, 0.4, 0.5])
+    dim_feedforward_encoder = trial.suggest_categorical('dim_feedforward_encoder', [32, 64, 128, 256, 512, 1024, 2048])
+    dim_feedforward_decoder = trial.suggest_categorical('dim_feedforward_decoder', [32, 64, 128, 256, 512, 1024, 2048])
+    batch_size = trial.suggest_categorical('batch_size', [32, 64])
+
+    input_seq_length = 7
+    output_seq_length = 7
+
+    data = "weather_data_full.csv"
+    target_features = [
+        "Average Daily Total Cloud Cover [oktas]",
+        "Average Daily Wind Speed [m/s]",
+        "Average Daily Temperature [°C]",
+        "Average Daily Relative Humidity [%]",
+        "Average Daily Station-Level Pressure [hPa]",
+        "Daily Precipitation Sum [mm]",
+        "Night Precipitation Sum [mm]"
+    ]
+    train_loader, val_loader, test_loader, X, Y = prepare_data(
+        data, input_seq_length, output_seq_length, batch_size, target_features
+    )
+
+    model = WeatherModel(
+        input_dim=X.shape[2],
+        embed_dim=embed_dim,
+        num_heads=num_heads,
+        num_layers=num_layers,
+        seq_len=input_seq_length,
+        output_features=Y.shape[2],
+        dropout=dropout,
+        dim_feedforward_encoder=dim_feedforward_encoder,
+        dim_feedforward_decoder=dim_feedforward_decoder
+    )
+
+    _, _, val_losses = train_model(model, train_loader, val_loader, num_epochs=15, device=device,
+                                   learning_rate=learning_rate)
+    return val_losses[-1]
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    input_seq_length = 7
+    output_seq_length = 7
+
+    data = "weather_data_full.csv"
+    target_features = [
+        "Average Daily Total Cloud Cover [oktas]",
+        "Average Daily Wind Speed [m/s]",
+        "Average Daily Temperature [°C]",
+        "Average Daily Relative Humidity [%]",
+        "Average Daily Station-Level Pressure [hPa]",
+        "Daily Precipitation Sum [mm]",
+        "Night Precipitation Sum [mm]"
+    ]
+
+    study = optuna.create_study(direction="minimize", study_name="weather_study")
     study.optimize(objective, n_trials=200)
 
     print(f"Best hyperparameters: {study.best_params}")
@@ -226,38 +284,19 @@ def main():
     b_dim_feedforward_decoder = best_params['dim_feedforward_decoder']
     b_batch_size = best_params['batch_size']
 
+    train_loader, val_loader, test_loader, X, Y = prepare_data(data,input_seq_length,output_seq_length,b_batch_size,target_features)
+
     model = WeatherModel(input_dim=X.shape[2], embed_dim=b_embed_dim, num_heads=b_num_heads, num_layers=b_num_layers,
-                         seq_len=input_seq_length,
-                         output_features=Y.shape[2], dropout=b_dropout, dim_feedforward_encoder=b_dim_feedforward_encoder,
-                         dim_feedforward_decoder=b_dim_feedforward_decoder)
+                             seq_len=input_seq_length,
+                             output_features=Y.shape[2], dropout=b_dropout, dim_feedforward_encoder=b_dim_feedforward_encoder,
+                             dim_feedforward_decoder=b_dim_feedforward_decoder)
 
-    # plt.figure(figsize=(10, 7))
-    # plt.plot(train_losses, label="Train Loss", color="blue")
-    # plt.plot(val_losses, label="Validation Loss", color="red")
-    # plt.xlabel("Epoch")
-    # plt.ylabel("Loss")
-    # plt.title("Training and Testing Loss")
-    # plt.legend()
-    # plt.show()
-    #
-    # all_predictions = np.concatenate(all_predictions, axis=0)
-    # all_targets = np.concatenate(all_targets, axis=0)
-    #
-    # num_samples = 5
-    #
-    # plt.figure(figsize=(12, 7))
-    #
-    # for i in range(num_samples):
-    #     plt.subplot(num_samples, 1, i + 1)
-    #     plt.plot(all_targets[i, :, 2], label="Actual", color="green")
-    #     plt.plot(all_predictions[i, :, 2], label="Predicted", linestyle="dashed", color="orange")
-    #     plt.ylabel("Value")
-    #     plt.legend()
-    #     plt.title(f"Sample {i + 1}")
-    #
-    # plt.xlabel("Time Step")
-    # plt.show()
 
+    trained_model, train_losses, val_losses = train_model(model, train_loader, val_loader, num_epochs=30, device=device, learning_rate=b_learning_rate)
+    test_loss, all_predictions, all_targets = evaluate_model(trained_model, test_loader, device)
+    visualisation(train_losses, val_losses, all_predictions, all_targets)
+
+    torch.save(trained_model, "best_model_full.pth")
 
 if __name__ == "__main__":
     main()
